54fd974ac47e9812a39f59e2c80795f0be3f9c23
amelie-girard <amelie.cj.girard@gmail.com>
2019-10-25 10:41:38 +0200

    Initial Commit (avec modif des méthodes simples)

---
+++ java/Yatzy.java
@@ -0,0 +1,211 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Yatzy {
+
+    protected List<Integer> dices;
+    public Yatzy(int d1, int d2, int d3, int d4, int d5)
+    {
+        dices = Arrays.asList(d1, d2, d3, d4, d5);
+    }
+
+    public int chance()
+    {
+        int []total = {0};
+      dices.forEach( dice -> total[0] += dice);
+        return total[0];
+    }
+
+    public int yatzy()
+    {
+        //Si la taille de distinctDiceest egale à 1 cela signifie que tous les dés sont égaux
+        List<Integer> distinctDice = dices.stream().distinct().collect(Collectors.toList());
+        return distinctDice.size() == 1 ? 50 : 0;
+    }
+
+    public int ones()
+    {
+        int []sum = {0};
+        dices.stream().filter(dice -> dice == 1).forEach( dice -> sum[0]+= dice);
+        return sum[0];
+    }
+
+    public int twos()
+    {
+        int []sum = {0};
+        dices.stream().filter(dice -> dice == 2).forEach( dice -> sum[0]+= dice);
+        return sum[0];
+    }
+
+    public int threes()
+    {
+        int []sum = {0};
+        dices.stream().filter(dice -> dice == 3).forEach( dice -> sum[0]+= dice);
+        return sum[0];
+    }
+
+    public int fours()
+    {
+        int []sum = {0};
+        dices.stream().filter(dice -> dice == 4).forEach( dice ->sum[0]+= dice);
+        return sum[0];
+    }
+
+    public int fives()
+    {
+        int []sum = {0};
+        dices.stream().filter(dice -> dice == 5).forEach( dice ->sum[0]+= dice);
+        return sum[0];
+    }
+
+    public int sixes()
+    {
+        int []sum = {0};
+        dices.stream().filter(dice -> dice == 6).forEach( dice -> sum[0]+= dice);
+        return sum[0];
+    }
+
+    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
+    {
+        int[] counts = new int[6];
+        counts[d1-1]++;
+        counts[d2-1]++;
+        counts[d3-1]++;
+        counts[d4-1]++;
+        counts[d5-1]++;
+        int at;
+        for (at = 0; at != 6; at++)
+            if (counts[6-at-1] >= 2)
+                return (6-at)*2;
+        return 0;
+    }
+
+    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
+    {
+        int[] counts = new int[6];
+        counts[d1-1]++;
+        counts[d2-1]++;
+        counts[d3-1]++;
+        counts[d4-1]++;
+        counts[d5-1]++;
+        int n = 0;
+        int score = 0;
+        for (int i = 0; i < 6; i += 1)
+            if (counts[6-i-1] >= 2) {
+                n++;
+                score += (6-i);
+            }
+        if (n == 2)
+            return score * 2;
+        else
+            return 0;
+    }
+
+    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
+    {
+        int[] tallies;
+        tallies = new int[6];
+        tallies[_1-1]++;
+        tallies[_2-1]++;
+        tallies[d3-1]++;
+        tallies[d4-1]++;
+        tallies[d5-1]++;
+        for (int i = 0; i < 6; i++)
+            if (tallies[i] >= 4)
+                return (i+1) * 4;
+        return 0;
+    }
+
+    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
+    {
+        int[] t;
+        t = new int[6];
+        t[d1-1]++;
+        t[d2-1]++;
+        t[d3-1]++;
+        t[d4-1]++;
+        t[d5-1]++;
+        for (int i = 0; i < 6; i++)
+            if (t[i] >= 3)
+                return (i+1) * 3;
+        return 0;
+    }
+
+    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
+    {
+        
+        int[] tallies;
+        tallies = new int[6];
+        tallies[d1-1] += 1;
+        tallies[d2-1] += 1;
+        tallies[d3-1] += 1;
+        tallies[d4-1] += 1;
+        tallies[d5-1] += 1;
+        if (tallies[0] == 1 &&
+            tallies[1] == 1 &&
+            tallies[2] == 1 &&
+            tallies[3] == 1 &&
+            tallies[4] == 1)
+            return 15;
+        return 0;
+    }
+
+    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
+    {
+        int[] tallies;
+        tallies = new int[6];
+        tallies[d1-1] += 1;
+        tallies[d2-1] += 1;
+        tallies[d3-1] += 1;
+        tallies[d4-1] += 1;
+        tallies[d5-1] += 1;
+        if (tallies[1] == 1 &&
+            tallies[2] == 1 &&
+            tallies[3] == 1 &&
+            tallies[4] == 1
+            && tallies[5] == 1)
+            return 20;
+        return 0;
+    }
+
+    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
+    {
+        int[] tallies;
+        boolean _2 = false;
+        int i;
+        int _2_at = 0;
+        boolean _3 = false;
+        int _3_at = 0;
+
+
+
+
+        tallies = new int[6];
+        tallies[d1-1] += 1;
+        tallies[d2-1] += 1;
+        tallies[d3-1] += 1;
+        tallies[d4-1] += 1;
+        tallies[d5-1] += 1;
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 2) {
+                _2 = true;
+                _2_at = i+1;
+            }
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 3) {
+                _3 = true;
+                _3_at = i+1;
+            }
+
+        if (_2 && _3)
+            return _2_at * 2 + _3_at * 3;
+        else
+            return 0;
+    }
+}
+
+
+
