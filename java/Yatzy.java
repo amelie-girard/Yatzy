88463623f5f136ba6c4e247eac4c44b9e4142226
amelie-girard <amelie.cj.girard@gmail.com>
2019-10-25 11:04:57 +0200

    S'il n'y a que 2 dés differents cela signifie que l'on a forcément 3 dès d'un nombre et 2 de l'autre. Il suffit ensuite de les ajouter

--- java/Yatzy.java
+++ java/Yatzy.java
@@ -164,41 +164,16 @@ public class Yatzy {
     }
 
 
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
+    public  int fullHouse()
     {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
+        List<Integer> distinctDice = dices.stream().distinct().collect(Collectors.toList());
+        if (distinctDice.size() == 2 ){
+            int []sum = {0};
+            dices.forEach(dice -> sum[0] += dice);
+            return sum[0];
+        } else
             return 0;
+
     }
 }
 
